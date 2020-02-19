e8a62fa5297ac6f609b263eeb5ed1bcd215c3d5f
Benito J. Gonzalez <bgonzalez@unicon.net>
2020-02-18 18:11:00 -0800

    fix(project.clj): add :scm and make a snapshot

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject org.clojars.bjagg/eve-api-client "1.3.8"
+(defproject org.clojars.bjagg/eve-api-client "1.3.8-SNAPSHOT"
   :description "An OpenAPI for EVE Online -- Clojure Client"
   :url "https://github.com/bjagg/eve-api-client"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :scm {:name "git"
+        :url "https://github.com/bjagg/eve-api-client"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "3.6.0"]
                  [cheshire "5.5.0"]]
