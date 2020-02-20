19d6d0ed55cf58ec7a374401e8e801e2a30ec99f
Benito J. Gonzalez <bgonzalez@unicon.net>
2020-02-20 09:18:59 -0800

    fix(project.clj): updated scm, clojure to 1.10.1, moved pom.xml to target/

--- project.clj
+++ project.clj
@@ -4,11 +4,13 @@
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :scm {:name "git"
-        :url "https://github.com/bjagg/eve-api-client"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+        :url "https://github.com/bjagg/eve-api-client"
+        :developerConnection "scm:git:git@github.com:bjagg/eve-api-client.git"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [clj-http "3.6.0"]
                  [cheshire "5.5.0"]]
   :deploy-repositories [[ "clojars" {:url "https://repo.clojars.org"
                                      :creds :gpg}]
                         ["releases" :clojars]
-                        ["snapshots" :clojars]])
+                        ["snapshots" :clojars]]
+  :pom-location "target/")
