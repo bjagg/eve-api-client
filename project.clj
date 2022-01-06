a72928155c2f87b6cb1a6d074b2580825cea51fb
Benito J. Gonzalez <bgonzalez@unicon.net>
2022-01-05 16:29:54 -0800

    chore: update deps and version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.jaggedcode/eve-api-client "1.10.1.1-SNAPSHOT"
+(defproject com.jaggedcode/eve-api-client "1.10.1.1"
   :description "An OpenAPI for EVE Online -- Clojure Client"
   :url "https://github.com/bjagg/eve-api-client"
   :license {:name "Apache License 2.0"
@@ -7,8 +7,8 @@
         :url "https://github.com/bjagg/eve-api-client"
         :developerConnection "scm:git:git@github.com:bjagg/eve-api-client.git"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [clj-http "3.6.0"]
-                 [cheshire "5.5.0"]]
+                 [clj-http "3.12.3"]
+                 [cheshire "5.10.1"]]
   :deploy-repositories [[ "clojars" {:url "https://repo.clojars.org/"
                                      :creds :gpg}]
                         ["releases" :clojars]
